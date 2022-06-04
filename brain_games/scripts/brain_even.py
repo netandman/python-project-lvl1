a6a3810ef7efbca530cae07e0e13fbe0d9b91169
netandman <detyuchenkoma@gmail.com>
2022-06-04 17:24:53 +0300

    The 5th step of Hexlet project lvl1

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,42 @@
+#!/user/bin/env python
+
+import prompt
+from random import randint
+
+
+def greeting_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    return name
+
+
+def main():
+    attempts = 0
+    user = greeting_user()
+    while attempts < 3:
+        number = randint(0, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if number % 2 == 0:
+            if answer == 'yes':
+                print('Correct!')
+                attempts += 1
+            else:
+                print(f'{answer} is wrong answer ;(. Correct answer was "no".')
+                break
+        elif number % 2 != 0:
+            if answer == 'no':
+                print('Correct!')
+                attempts += 1
+            else:
+                print(f'{answer} is wrong answer ;(. Correct answer was "yes".')
+                break
+    else:
+        print(f'Congratulation, {user}!')
+    return
+
+
+if __name__ == '__main__':
+    main()
